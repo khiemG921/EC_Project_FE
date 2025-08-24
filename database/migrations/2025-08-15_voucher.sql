b620bfb024465dc9501ce97c8dd10fe91ba7089a
Phạm Hoàng Tiên <phamhoangtien4444@gmail.com>
2025-08-24 16:32:28 +0700

    delete some files

--- database/migrations/2025-08-15_voucher.sql
+++
@@ -1,4 +0,0 @@
-USE e_commerce;
-
-ALTER TABLE voucher
-    ADD COLUMN IF NOT EXISTS used_count INT DEFAULT 0;
--- store/types/voucher_usage.ts
+++
@@ -1,6 +0,0 @@
-export type VoucherUsage = {
-  voucher_usage_id: number;
-  voucher_id: number;
-  transaction_id?: string;
-  used_at: string; // ISO date
-};
